c4d00bce1eefe4930e81796e9479b5f9a46e3829
tamaki annen <tmn.agenkir.luck@icloud.com>
2021-06-17 11:08:04 +0900

    DesignHomeViewまで完成！

--- Portfolio/SellectViewController.swift
+++ Portfolio/SellectViewController.swift
@@ -17,6 +17,9 @@ class SellectViewController: UIViewController {
     @IBOutlet weak var View1: UIImageView!
     @IBOutlet weak var View2: UIImageView!
     
+    var t1: [UIImage]!
+    var t2: [UIImage]!
+    var y1: [UIImage]!
     var y2: [UIImage]!
     
     override func viewDidLoad() {
@@ -29,6 +32,24 @@ class SellectViewController: UIViewController {
     
     @IBAction func tappedsegmentChanged(sender: UISegmentedControl) {
         
+        t1 = [UIImage(named:"t11.jpg")!,
+              UIImage(named:"t12.jpg")!,
+              UIImage(named:"t13.jpg")!,
+              UIImage(named:"t14.jpg")!,
+              UIImage(named:"t15.jpg")!,]
+        
+        t2 = [UIImage(named:"t21.jpg")!,
+              UIImage(named:"t22.jpg")!,
+              UIImage(named:"t23.jpg")!,
+              UIImage(named:"t24.jpg")!,
+              UIImage(named:"t25.jpg")!,]
+        
+        y1 = [UIImage(named:"y11.jpg")!,
+              UIImage(named:"y12.jpg")!,
+              UIImage(named:"y13.jpg")!,
+              UIImage(named:"y14.jpg")!,
+              UIImage(named:"y15.jpg")!,]
+        
         y2 = [UIImage(named:"y21.jpg")!,
               UIImage(named:"y22.jpg")!,
               UIImage(named:"y23.jpg")!,
@@ -40,14 +61,14 @@ class SellectViewController: UIViewController {
             print("A4横")
             self.DisplayLabel1.text = "A4横1"
             self.DisplayLabel2.text = "A4横2"
-            self.View1.image = UIImage(named: "A4yoko.png")!
-            self.View2.image = UIImage(named: "A4yoko2.png")
+            self.View1.image = y1[0]
+            self.View2.image = y2[0]
         case 1:
             print("A4縦")
             self.DisplayLabel1.text = "A4縦1"
             self.DisplayLabel1.text = "A4縦2"
-            self.View1.image = UIImage(named: "A4tate.png")!
-            self.View2.image = y2[0]
+            self.View1.image = t1[0]
+            self.View2.image = t2[0]
         default:
             print("選択されたもの：未選択")
             self.DisplayLabel1.text = "未選択"
@@ -70,13 +91,25 @@ class SellectViewController: UIViewController {
               let nextVC = segue.destination as? DesignHomeViewController
             nextVC?.outputValue = self.View1.image
              // nextVC?.outputValue = self.inputField.text
-          }
-        if segue.identifier == "select2" {
+            
+            let next = segue.destination as? DesignHomeViewController
+            if View1.image == t1[0]{
+              next?.output = t1
+            } else if View1.image == y1[0]{
+                next?.output = y1
+            }
+            
+          }else if segue.identifier == "select2" {
             let nextVC = segue.destination as? DesignHomeViewController
           nextVC?.outputValue = self.View2.image
             
             let next = segue.destination as? DesignHomeViewController
-          next?.output = y2
+            
+            if View2.image == t2[0]{
+              next?.output = t2
+            } else if View2.image == y2[0]{
+                next?.output = y2
+            }
            // nextVC?.outputValue = self.inputField.text
         }
       }
